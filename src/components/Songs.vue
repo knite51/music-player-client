befe83364bd6a98917eb0c7faa57d6cb6127496e
KniteDev <bmose@atbtechsoft.com>
2019-04-21 09:49:31 +0100

    create songs component and route

---
+++ src/components/Songs.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout class="songsBox">
+      <v-flex xs12 sm8 offset-sm2>
+        <my-awesome-panel title="Songs"></my-awesome-panel>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import MyAwesomePanel from "@/components/Panel";
+
+export default {
+  components: {
+    MyAwesomePanel
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style scoped>
+</style>
