e4e05585d28cf551b07cf83f6c34d4b0b624e603
KniteDev <bmose@atbtechsoft.com>
2019-04-21 09:47:55 +0100

    add vuex to application

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import Vuetify from 'vuetify';
+import { sync } from 'vuex-router-sync';
 import 'vuetify/dist/vuetify.min.css';
+import store from './store/store';
 
 Vue.use(Vuetify);
+sync(store, router);
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app');
