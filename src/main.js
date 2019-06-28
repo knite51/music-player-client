c5a7269e4f476ab5e00be3da92bdf4020288508b
kniteDeveloper <knite51@gmail.com>
2019-06-29 01:24:41 +0200

    add youtube-embed

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import Vuetify from 'vuetify';
-import { sync } from 'vuex-router-sync';
-import 'vuetify/dist/vuetify.min.css';
-import store from './store/store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Vuetify from "vuetify";
+import { sync } from "vuex-router-sync";
+import "vuetify/dist/vuetify.min.css";
+import store from "./store/store";
+import VueYouTubeEmbed from "vue-youtube-embed";
+
+Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
+
+Vue.use(VueYouTubeEmbed);
+
 sync(store, router);
-Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
   router,
-  store
-}).$mount('#app');
+  store,
+}).$mount("#app");
