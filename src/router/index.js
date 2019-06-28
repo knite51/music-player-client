cb70d7acf317037b9d754c2bfe60f95b10ed86ee
kniteDeveloper <knite51@gmail.com>
2019-06-29 01:28:31 +0200

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,57 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
-import Register from '@/components/Register';
-import Login from '@/components/Login';
-import Songs from '@/components/Songs';
-import SongsForm from '@/components/SongsForm';
-import SongView from '@/components/ViewSong/Index';
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Register from "@/components/Register";
+import Login from "@/components/Login";
+import Songs from "@/components/Songs";
+import CreateSong from "@/components/CreateSong";
+import SongView from "@/components/ViewSong/Index";
+import EditSong from "@/components/EditSong";
+import SpeechView from "@/components/Speech/SpeechView";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'root',
-      component: HelloWorld
+      path: "/",
+      name: "root",
+      component: HelloWorld,
     },
     {
-      path: '/register',
-      name: 'register',
-      component: Register
+      path: "/register",
+      name: "register",
+      component: Register,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: Login
+      path: "/login",
+      name: "login",
+      component: Login,
     },
     {
-      path: '/songs',
-      name: 'songs',
-      component: Songs
+      path: "/songs",
+      name: "songs",
+      component: Songs,
     },
     {
-      path: '/songs/create',
-      name: 'songsForm',
-      component: SongsForm
+      path: "/songs/create",
+      name: "createSong",
+      component: CreateSong,
     },
     {
-      path: '/song/:songID',
-      name: 'songView',
-      component: SongView
-    }
-  ]
+      path: "/song/:songID",
+      name: "songView",
+      component: SongView,
+    },
+    {
+      path: "/song/:songID/edit",
+      name: "editSong",
+      component: EditSong,
+    },
+    {
+      path: "/speechService",
+      name: "speechApi",
+      component: SpeechView,
+    },
+  ],
 });
